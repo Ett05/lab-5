a5d3e32804ffd0f98d9336c70e98a8dba26d6f21
Abdallah-Abdelaal <abdallahabdelaal2005@gmail.com>
2024-10-23 22:16:26 -0400

    Abdallah

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -8,7 +8,6 @@ public class LogoutInputData {
     private String name;
 
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
         this.name = username;
     }
 
